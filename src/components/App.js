f2ecf85ebb7784438cfb43b5a2e012b74bb48131
hikmanisyariful <syarifulfajar017@gmail.com>
2021-08-14 14:37:02 +0700

    adding feature loadingBar using react-redux-loading-package

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading";
 
 class App extends Component {
   componentDidMount() {
@@ -11,10 +12,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Dashboard />
+        <LoadingBar />
+        {this.props.loading === true ? null : <Dashboard />}
       </div>
     );
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  };
+}
+
+export default connect(mapStateToProps)(App);
